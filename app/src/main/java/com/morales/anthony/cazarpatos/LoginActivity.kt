ab8cc44de93a7157a2339932c2b8735a571a4f14
P3E004-K <P3E004-K@DESKTOP-E20A818>
2025-05-16 15:08:13 -0500

    Added login activity

--- app/src/main/java/com/morales/anthony/cazarpatos/LoginActivity.kt
+++ app/src/main/java/com/morales/anthony/cazarpatos/LoginActivity.kt
@@ -1,34 +1,20 @@
 package com.morales.anthony.cazarpatos
 
-import android.content.Intent
 import android.os.Bundle
-import android.util.Log
-import android.widget.Button
-import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
-    //1. Declarar la variable
-    private lateinit var buttonLogin: Button
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        //2.Inicializar la variable
-        buttonLogin =findViewById(R.id.buttonLogin)
+        setContentView(R.layout.activity_login)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-        //3. Usar la variable
-        buttonLogin.setOnClickListener {
-            Toast.makeText(this, getString(R.string.buttonLogin_Message), Toast.LENGTH_SHORT).show()
-            val intent = Intent(this, LoginActivity::class.java)
-            startActivity(intent)
         }
     }
 }
